fce7fd6fdd4cf946ea45e6b727e959aa43655bc7
Raphael Maurício <raphaelmauricio12@gmail.com>
2025-08-25 09:06:12 -0300

    [FIX] Refatorando o código para receber DiscountCalculator com as dependências injetadas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
-import br.com.mariojp.solid.ocp.*;
+import br.com.mariojp.solid.ocp.CustomerType;
+import br.com.mariojp.solid.ocp.DependencyContainer;
+import br.com.mariojp.solid.ocp.DiscountCalculator;
 
 public class Main {
 	public static void main(String[] args) {
-		DiscountCalculator calc = new DiscountCalculator();
+		DependencyContainer container = new  DependencyContainer();
+
+		DiscountCalculator calc = container.getDiscountCalculator();
 		System.out.println("REGULAR 100 -> " + calc.apply(100, CustomerType.REGULAR));
 		System.out.println("PREMIUM 100 -> " + calc.apply(100, CustomerType.PREMIUM));
 		// PARTNER deveria ter 12%
--- src/test/java/br/com/mariojp/solid/ocp/DiscountCalculatorTest.java
+++ src/test/java/br/com/mariojp/solid/ocp/DiscountCalculatorTest.java
@@ -1,12 +1,14 @@
 package br.com.mariojp.solid.ocp;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class DiscountCalculatorTest {
     @Test
     void partner_gets_12_percent() {
-        var calc = new DiscountCalculator();
+		DependencyContainer container = new  DependencyContainer();
+        
+        var calc = container.getDiscountCalculator();
         assertEquals(88.0, calc.apply(100.0, CustomerType.PARTNER), 0.0001,
             "PARTNER deveria ter 12% de desconto");
     }
